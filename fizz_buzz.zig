c049f2db2f66d06994560f008f3de034a72979fe
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-02-26 21:41:38 +0300

    using switch statement

--- fizz_buzz.zig
+++ fizz_buzz.zig
@@ -2,8 +2,16 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print(
-        \\Hello lahatrad
-        \\
-    , .{});
+    var count: u8 = 1;
+    while (count <= 100) : (count += 1) {
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+
+        switch (div_3 * 2 + div_5) {
+            0b00 => try stdout.print("{}\n", .{count}),
+            0b01 => try stdout.print("Buzz\n", .{}),
+            0b10 => try stdout.print("Fizz\n", .{}),
+            0b11 => try stdout.print("Fizz Buzz\n", .{}),
+        }
+    }
 }
