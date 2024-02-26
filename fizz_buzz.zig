16c2ea763285cbeb7a0510c9ea8abcc8619dc7a4
lahatra3 <lahatrap20.aps2a@gmail.com>
2024-02-26 20:22:13 +0300

    using if statement...

--- fizz_buzz.zig
+++ fizz_buzz.zig
@@ -2,8 +2,16 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print(
-        \\Hello lahatrad
-        \\
-    , .{});
+    var count: u8 = 1;
+    while (count <= 100) : (count += 1) {
+        if (count % 3 == 0 and count % 5 == 0) {
+            try stdout.print("Fizz Buzz\n", .{});
+        } else if (count % 3 == 0) {
+            try stdout.print("Fizz\n", .{});
+        } else if (count % 5 == 0) {
+            try stdout.print("Buzz\n", .{});
+        } else {
+            try stdout.print("{}\n", .{count});
+        }
+    }
 }
