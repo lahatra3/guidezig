28c19ea051fc76eb977a508e7e15c0731c0af2bb
lahatra3 <lahatra.ravelonarivo@gmail.com>
2024-02-28 20:24:28 +0300

    feat(fizz_buzz): merging if_statement into main...

--- fizz_buzz.zig
+++ fizz_buzz.zig
@@ -1,9 +1,33 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
     try stdout.print(
         \\Hello lahatrad
         \\
     , .{});
+	try stdout.print(
+		\\
+		\\******* USING IF STATEMENT *******
+		\\
+		, .{});
+	try using_if_statement();
 }
+
+fn using_if_statement() !void {
+	var count: u8 = 1;
+	while (count <= 100): (count += 1) {
+		if (count % 3 == 0 and count % 5 == 0) {
+			try stdout.print("Fizz Buzz\n", .{});
+		}
+		else if (count % 3 == 0) {
+			try stdout.print("Fizz\n", .{});
+		}
+		else if (count % 5 == 0) {
+			try stdout.print("Buzz\n", .{});
+		}
+		else try stdout.print("{}\n", .{count});
+	}
+}
+
+
