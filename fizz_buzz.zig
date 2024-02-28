72fbba50dbe92eaa6f73e92e1c52497a9727d761
lahatra3 <lahatra.ravelonarivo@gmail.com>
2024-02-28 21:03:50 +0300

    feat(fizz_buzz): merging others metthods into main...

--- fizz_buzz.zig
+++ fizz_buzz.zig
@@ -6,28 +6,67 @@ pub fn main() !void {
         \\Hello lahatrad
         \\
     , .{});
-	try stdout.print(
-		\\
-		\\******* USING IF STATEMENT *******
-		\\
-		, .{});
-	try using_if_statement();
+    try stdout.print(
+        \\
+        \\******* USING IF STATEMENT *******
+        \\
+    , .{});
+    try using_if_statement();
+
+    try stdout.print(
+        \\
+        \\******* USING SWITCH STATEMENT *******
+        \\
+    , .{});
+    try using_switch_statement();
+
+    try stdout.print(
+        \\
+        \\******* USING BITEWISE OPERATION *******
+        \\
+    , .{});
+    try using_bitewise_operation();
 }
 
 fn using_if_statement() !void {
-	var count: u8 = 1;
-	while (count <= 100): (count += 1) {
-		if (count % 3 == 0 and count % 5 == 0) {
-			try stdout.print("Fizz Buzz\n", .{});
-		}
-		else if (count % 3 == 0) {
-			try stdout.print("Fizz\n", .{});
-		}
-		else if (count % 5 == 0) {
-			try stdout.print("Buzz\n", .{});
-		}
-		else try stdout.print("{}\n", .{count});
-	}
+    var count: u8 = 1;
+    while (count <= 100) : (count += 1) {
+        if (count % 3 == 0 and count % 5 == 0) {
+            try stdout.print("Fizz Buzz\n", .{});
+        } else if (count % 3 == 0) {
+            try stdout.print("Fizz\n", .{});
+        } else if (count % 5 == 0) {
+            try stdout.print("Buzz\n", .{});
+        } else try stdout.print("{}\n", .{count});
+    }
 }
 
+fn using_switch_statement() !void {
+    var count: u8 = 1;
+    while (count <= 100) : (count += 1) {
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
 
+        switch (div_3 * 2 + div_5) {
+            0b00 => try stdout.print("{}\n", .{count}),
+            0b01 => try stdout.print("Buzz\n", .{}),
+            0b10 => try stdout.print("Fizz\n", .{}),
+            0b11 => try stdout.print("Fizz Buzz\n", .{}),
+        }
+    }
+}
+
+fn using_bitewise_operation() !void {
+    var count: u8 = 1;
+    while (count <= 100) : (count += 1) {
+        const div_3: u2 = @intFromBool(count % 3 == 0);
+        const div_5 = @intFromBool(count % 5 == 0);
+
+        switch (div_3 << 1 | div_5) {
+            0b00 => try stdout.print("{}\n", .{count}),
+            0b01 => try stdout.print("Buzz\n", .{}),
+            0b10 => try stdout.print("Fizz\n", .{}),
+            0b11 => try stdout.print("Fizz Buzz\n", .{}),
+        }
+    }
+}
